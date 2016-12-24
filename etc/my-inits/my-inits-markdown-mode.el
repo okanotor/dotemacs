3964a9760574b004aa4245c9fc8b5f3aeda3000b
Tooru Okano <transparency1979@gmail.com>
2016-12-24 19:06:15 +0900

    表の編集にorgtbl-modeを使用するよう修正

--- etc/my-inits/my-inits-markdown-mode.el
+++ etc/my-inits/my-inits-markdown-mode.el
@@ -8,4 +8,14 @@
 (autoload 'markdown-mode "markdown-mode.el" "Major mode for editing Markdown files" t)
 (add-to-list 'auto-mode-alist '("\\.md$" . markdown-mode))
 
+;; @ref http://qiita.com/nanasess/items/c9342c06a3e28e64aeb8
+(defun cleanup-org-tables ()
+  (save-excursion
+    (goto-char (point-min))
+    (while (search-forward "-+-" nil t) (replace-match "-|-"))))
+(add-hook 'markdown-mode-hook 'orgtbl-mode)
+(add-hook 'markdown-mode-hook
+          #'(lambda()
+          (add-hook 'after-save-hook 'cleanup-org-tables  nil 'make-it-local)))
+
 ;;; my-inits-markdown-mode.el ends here
