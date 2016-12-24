d5ecfb5373de47f47277505aded21716410fc7bc
Tooru Okano <transparency1979@gmail.com>
2016-12-24 19:07:30 +0900

    特定のファイルの場合に、操作説明のコメントを挿入するよう修正。

--- etc/my-inits/my-inits-open-junk-file.el
+++ etc/my-inits/my-inits-open-junk-file.el
@@ -15,12 +15,21 @@
   ;; - 挿入時にコメントを記入できるよう改良
   ;; - リストにない拡張子の場合にエラーになる不具合を修正
   ;; - リストは自分用に変更
+  ;; - 備忘録を出力できるよう修正
   ;; @see http://d.hatena.ne.jp/hotoku/20130301/1362148309
   (defvar open-junk-ext-tags-alist
     '(("el" ";;" "ELISP")
-      ("r" "#" "R")
-      ("rb" "#" "RUBY")
-      ("sql" "--" "SQL")
+      ("r" "#" "R" ("- R 起動 => M-x R"
+                    "- 編集中のコードを実行 => C-c C-c"))
+      ("rb" "#" "RUBY" ("- inf-ruby 起動 => M-x inf-ruby"
+                        "- robe 起動 (事前に inf-ruby を起動しておく) => M-x robe-start"
+                        "- rcodetools関連"
+                        "  - コメント挿入 => M-x を2回"
+                        "  - xmpfilter実行 => C-Return"))
+      ("sql" "--" "SQL" ("- SQLクライアント起動 => M-x my-sql-connect"
+                         "  - 接続情報は sql-connection-alist に書いておく"
+                         "- 編集中のSQLを実行 => C-c C-c"
+                         "- 選択中のリージョンのSQLを実行 => C-c C-r"))
       ("scm" ";;" "SCHEME")))
   (defadvice open-junk-file
     (after open-junk-file-insert-howm-comment-advice activate)
@@ -30,8 +39,14 @@ to be searched by howm."
            (asc (assoc ext open-junk-ext-tags-alist)))
       (when asc
         (let* ((prefix (cadr asc))
-               (tag (caddr asc)))
-          (insert prefix " %" tag " " (read-string "COMMENT: ") "\n")))))
+               (tag (caddr asc))
+               (memo (cadddr asc)))
+          (insert prefix " %" tag " " (read-string "COMMENT: ") "\n")
+          (when memo
+            (insert prefix "\n")
+            (dolist (text memo)
+              (insert prefix " " text "\n")))
+          ))))
 
   ;; elisp ファイルの場合、lisp-interaction-mode で開く
   (defadvice open-junk-file (after open-junk-file-emacs-lisp-advice activate)
